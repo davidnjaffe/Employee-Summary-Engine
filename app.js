a437e472c43fbbb4c339de4501d7bb2a03df53e2
David <Jaffe>
2020-09-22 17:17:47 -0700

    working application with assets

--- app.js
+++ app.js
@@ -4,14 +4,54 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-// const {render, renderManager} = require("./lib/htmlRenderer");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const { run } = require("jest");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const devTeam = [];
+
+function createDevTeam() {
+
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "memberType",
+        message: "What role would you like to add to the team?",
+        choices: [
+          "Manager",
+          "Engineer",
+          "Intern",
+          "No additional team members"
+        ]
+      }
+    ]).then(answer => {
+      switch(answer.memberType) {
+        case "Manager":
+            createManager();
+            break;
+        case "Engineer":
+            createEngineer();
+            break;
+        case "Intern":
+            createIntern();
+            break;
+        default:
+            runApp();
+      }
+    });
+
+}
+
+// createDevTeam();
+
+function createManager() {
 inquirer.prompt(
     [
         {
@@ -44,12 +84,117 @@ inquirer.prompt(
         }
     ]
 ).then(function(answers){
-    console.log(answers)
+    // console.log(answers)
     const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerNumber)
-    console.log(manager)
+    // console.log(manager)
+    devTeam.push(manager)
+    createDevTeam();
     // const managerHtml = renderManager(manager);
     // console.log(managerHtml);
-})
+    });
+}
+
+// function for gathering input for engineer role
+
+function createEngineer(){
+    inquirer.prompt(
+        [
+            {
+            type: "input",
+            message: "Please give engineer's name",
+            name: "engineerName"
+            
+            
+            },
+            {
+            type: "input",
+            message: "Please give engineer's id",
+            name: "engineerId"
+                
+                
+            },
+            {
+            type: "input",
+            message: "Please give engineer's email",
+            name: "engineerEmail"
+                    
+                    
+            },
+            {
+             type: "input",
+            message: "Please give engineer's Github username",
+            name: "github"
+                        
+                        
+            }
+        ]
+    ).then(function(answers){
+        // console.log(answers)
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.github)
+        // console.log(engineer)
+        devTeam.push(engineer)
+        createDevTeam();
+        
+        });
+    }
+    
+    // function to gather information for intern role
+
+    function createIntern(){
+        inquirer.prompt(
+            [
+                {
+                type: "input",
+                message: "Please give intern's name",
+                name: "internName"
+                
+                
+                },
+                {
+                type: "input",
+                message: "Please give intern's id",
+                name: "internId"
+                    
+                    
+                },
+                {
+                type: "input",
+                message: "Please give intern's email",
+                name: "internEmail"
+                        
+                        
+                },
+                {
+                 type: "input",
+                message: "Please give intern's school",
+                name: "internSchool"
+                            
+                            
+                }
+            ]
+        ).then(function(answers){
+            // console.log(answers)
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+            // console.log(intern)
+            devTeam.push(intern)
+            createDevTeam();
+           
+            });
+        }
+        
+        // function to run the app and write roles to html
+
+        function runApp() {
+            console.log(devTeam);
+    
+            fs.writeFileSync(outputPath, render (devTeam), "utf-8");
+           
+        }
+        
+        createDevTeam();
+        
+
+        
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
